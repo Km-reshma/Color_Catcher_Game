6b14de47840515fe15f53650ad03afa330ef7bb4
Km-reshma <reshmak0811@gmail.com>
2026-07-21 19:53:47 +0530

    to define function for getRandomColor()

--- myscript.js
+++ myscript.js
@@ -1,3 +1,25 @@
+let targetcolor = "";
+let score = 0;                                    //score variable global and local =0 bcz we restart the game 
+let time =30;
+let timer;
+let colors =['red', 'orange', 'green', 'grey', 'black', 'hotpink', 'purple', 'yellow', 'teal', 'white', 'lightgreen', 'maroon', 'peach', 'khaki', 'navyblue', 'indigo'   ]
+const grid = document.getElementById("grid");
+const targetColorDisplay = document.getElementById("targetColor");
+const scoreDispley= document.getElementById("score");
+const timeDisplay= document.getElementById("time")                   // these are the object which is needed to use 
+
+// to create a function to generate the random color 
+
+function getRandomColor(){
+
+}
+
+// to create function for start the game 
 function startGame(){
+    score = 0;
+    time = 30 ;
+
+    
+     
 
 }
